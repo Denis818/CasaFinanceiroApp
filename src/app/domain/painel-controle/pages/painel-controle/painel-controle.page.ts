788745d64baaa00b06cec6253cae1624a705bc9d
Denis Developer <denispereiradosreis@gmail.com>
2024-04-27 20:49:31 -0300

    estilizando botões de vizualização

--- src/app/domain/painel-controle/pages/painel-controle/painel-controle.page.ts
+++ src/app/domain/painel-controle/pages/painel-controle/painel-controle.page.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatDialog } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 import { MatPaginatorIntl } from '@angular/material/paginator';
 import { CustomPaginator } from 'src/app/shared/utilities/paginator/custom-paginator';
 import { ModalCategoriaComponent } from '../../modal/create/categoria/create-categoria.modal';
@@ -20,6 +23,9 @@ import { ViewMembroModal } from '../../modal/view/view-membro-modal/view-membro.
     ViewCategoriaModal,
     ViewMembroModal,
     ViewDespesaModal,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
   ],
   providers: [{ provide: MatPaginatorIntl, useClass: CustomPaginator }],
 })
