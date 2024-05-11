1b2e4930d77ab573bf2530f0fd1d3a925fefbaef
Denis Developer <denispereiraa55@gmail.com>
2024-05-11 13:44:36 -0300

    revert

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  base_url_financy:
-    'https://fantastic-gentleness-production.up.railway.app/api/v1',
+  base_url_financy: 'https://localhost:7022/api/v1',
 };
