4c71a0fe8b892fee5bffcb8a67b515c7a8a10fe5
Denis Developer <denispereiraa55@gmail.com>
2024-06-08 13:57:56 -0300

    correção

--- src/app/domain/conferencia-compras/Components/painel-economia/painel-economia.component.ts
+++ src/app/domain/conferencia-compras/Components/painel-economia/painel-economia.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { SugestaoFornecedorComponent } from './sugestao-fornecedor/sugestao-fornecedor.component';
 import { GraficoSugestoesEconomiaComponent } from './grafico-sugestoes-economia/grafico-sugestoes-economia.component';
 
-
-
 @Component({
   selector: 'painel-economia',
   templateUrl: './painel-economia.component.html',
