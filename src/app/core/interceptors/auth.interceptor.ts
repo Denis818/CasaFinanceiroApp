404e33152d1b104eed8d18f325dcc87ed6666865
Denis Developer <denispereiraa55@gmail.com>
2024-08-18 13:45:57 -0300

    correção

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -29,14 +29,14 @@ export class AuthInterceptor implements HttpInterceptor {
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
     const token = this.storageService.getItem('token');
-    let faturaId = this.storageService.getItem('grupo-fatura-code');
-    faturaId = faturaId && parseInt(faturaId) !== 0 ? faturaId : null;
-
+    let faturaCode =
+      this.storageService.getItem('grupo-fatura-code') ??
+      '00000000-0000-0000-0000-000000000000';
 
     let headers = req.headers.set('Authorization', `Bearer ${token}`);
 
-    if (faturaId) {
-      headers = headers.set('grupo-fatura-code', faturaId);
+    if (faturaCode) {
+      headers = headers.set('grupo-fatura-code', faturaCode);
     }
 
     const authReq = req.clone({ headers });
