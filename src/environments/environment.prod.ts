eb4adffb3afd4dbc28a28364f45d87910bc29483
Denis Developer <denispereiraa55@gmail.com>
2024-10-20 16:34:16 -0300

    ajust

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   base_url_financy: 'https://casa-financeiro-api.up.railway.app/api/v1',
+  base_url_financy_v2: 'https://casa-financeiro-api.up.railway.app/api/v2',
 };
